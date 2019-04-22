c046c53fe5e199b3d201e0be841cd9921658d1d8
Song Cho <deeplearner507@gmail.com>
2019-04-22 23:32:28 +0900

    Adding a Button and Managing State

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Platform, StyleSheet, Button, View, TextInput, Text} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -11,7 +11,8 @@ const instructions = Platform.select({
 //type Props = {};
 export default class App extends Component {
   state ={
-    placeName:""
+    placeName:"",
+    places:[]
   }
 
   placeNameChangedHandler = (val) => {
@@ -21,14 +22,39 @@ export default class App extends Component {
     });
   };
 
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    })
+  }
+
   render() {
+    const placesOutput = this.state.places.map((place,index)=>(
+        <Text key={index}>
+          {place}
+        </Text>
+    ));
     return (
       <View style={styles.container}>
-        <TextInput 
-          style={{width: 300}}
-          placeholder="An awesome place"
-          value={this.state.placeName} 
-          onChangeText={this.placeNameChangedHandler} />
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={{width: 300}}
+            placeholder="An awesome places"
+            value={this.state.placeName} 
+            onChangeText={this.placeNameChangedHandler} 
+            style ={styles.placeInput}
+          />
+          <Button title="Add" style={styles.placeButton} onPress={this.placeSubmitHandler}/>
+        </View>
+        <View>
+          {placesOutput}
+        </View>
       </View>
     );
   }
@@ -41,5 +67,18 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#FFF',
+  },
+  inputContainer: {
+    //flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
   }
 });
