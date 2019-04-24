b30b7871975e57d9337754db8a4ae5a58551cce7
Song Cho <deeplearner507@gmail.com>
2019-04-24 23:15:20 +0900

    add delete item event

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const listItem = (props) => (
-    <TouchableWithoutFeedback onPress={props.onItemPressed}>
-        <View style={styles.listItem} >
-            <Text>{props.placeName}</Text>
-        </View>
-    </TouchableWithoutFeedback>
-
+const listItem = props => (
+  <TouchableOpacity onPress={props.onItemPressed}>
+    <View style={styles.listItem}>
+      <Text>{props.placeName}</Text>
+    </View>
+  </TouchableOpacity>
 );
 
-
 const styles = StyleSheet.create({
-    listItem: {
-        width:"100%",
-        padding: 10,
-        backgroundColor: "#eee",
-        marginBottom: 5
-    }
+  listItem: {
+    width: "100%",
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: "#eee"
+  }
 });
 
 export default listItem;
-
-
