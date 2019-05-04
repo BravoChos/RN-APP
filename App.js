07e4d9e24a00b093d1aa63216e74fc0f6291045e
Song Cho <deeplearner507@gmail.com>
2019-05-05 01:11:25 +0900

    implementing redux

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { Navigation } from "react-native-navigation";
+import { Provider } from "react-redux";
 
 import AuthScreen from "./src/screens/Auth/Auth";
 import SharePlace from "./src/screens/SharePlace/SharePlace";
 import FindPlace from "./src/screens/FindPlace/FindPlace";
+import configureStore from "./src/store/configureStore";
+
+const store = configureStore();
 
 // Register Screens
-Navigation.registerComponent("rnapp.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("rnapp.SharePlace", () => SharePlace);
-Navigation.registerComponent("rnapp.FindPlace", () => FindPlace);
+Navigation.registerComponent("rnapp.AuthScreen", () => AuthScreen, store, Provider);
+Navigation.registerComponent("rnapp.SharePlace", () => SharePlace, store, Provider);
+Navigation.registerComponent("rnapp.FindPlace", () => FindPlace, store, Provider);
 
 // Start a App
 Navigation.startSingleScreenApp({
