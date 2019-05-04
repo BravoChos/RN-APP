d90a7011955c7bcc9957fa609301ed29b2df5f20
Song Cho <deeplearner507@gmail.com>
2019-05-04 18:30:22 +0900

    add tabs with icons

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { Navigation } from "react-native-navigation";
 
 import AuthScreen from "./src/screens/Auth/Auth";
+import SharePlace from "./src/screens/SharePlace/SharePlace";
+import FindPlace from "./src/screens/FindPlace/FindPlace";
 
 // Register Screens
 Navigation.registerComponent("rnapp.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("rnapp.SharePlace", () => SharePlace);
+Navigation.registerComponent("rnapp.FindPlace", () => FindPlace);
 
 // Start a App
 Navigation.startSingleScreenApp({
---
+++ src/screens/SharePlace/SharePlace.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+
+class SharePleceScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    On SharePleceScreen
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default SharePleceScreen;
