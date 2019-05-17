64b6c268220250bbfe7f41c85901c713d1fff16b
Song Cho <deeplearner507@gmail.com>
2019-05-18 01:17:25 +0900

    Using the Validation state

--- src/components/UI/ButtonWithBackground/ButtonWithBackground.js
+++ src/components/UI/ButtonWithBackground/ButtonWithBackground.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { TouchableOpacity, Text, View, StyleSheet, TouchableNativeFeedback, Platform } from 'react-native';
 
 const buttonWithBackground = props => {
-    const content = (
-      <View style={[styles.button, { backgroundColor: props.color }]}>
-        <Text>{props.children}</Text>
-      </View>
-    );
+  const content = (
+    <View
+      style={[
+        styles.button,
+        { backgroundColor: props.color },
+        props.disabled ? styles.disabled : null
+      ]}
+    >
+      <Text style={props.disabled ? styles.disabledText : null}>
+        {props.children}
+      </Text>
+    </View>
+  );
+  if (props.disabled) {
+    return content;
+  }
     if (Platform.OS === "android") {
       return (
         <TouchableNativeFeedback onPress={props.onPress}>
@@ -24,6 +35,13 @@ const buttonWithBackground = props => {
       borderRadius: 5,
       borderWidth: 1,
       borderColor: "black"
+    },
+    disabled: {
+      backgroundColor: "#eee",
+      borderColor: "#aaa"
+    },
+    disabledText: {
+      color: "#aaa"
     }
   });
   
