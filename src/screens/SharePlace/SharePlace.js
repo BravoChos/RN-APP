5c8302d4a6e57abdea70fe1b964f3f5e00c47ebd
Song Cho <deeplearner507@gmail.com>
2019-05-22 01:10:43 +0900

    storing locations with redux

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -23,6 +23,10 @@ class SharePlaceScreen extends Component {
         touched: false,
         validationRules: {
           notEmpty: true
+        },
+        location: {
+          value: null,
+          valid: false
         }
       }
     }
@@ -59,10 +63,25 @@ class SharePlaceScreen extends Component {
       });
     };
 
+    locationPickedHandler = location => {
+      this.setState(prevState => {
+        return {
+          controls: {
+            ...prevState.controls,
+            location: {
+              value: location,
+              valid: true
+            }
+          }
+        };
+      });
+    };
+
     placeAddedHandler = () => {
-      if (this.state.controls.placeName.value.trim() !== "") {
-        this.props.onAddPlace(this.state.controls.placeName.value);
-      }
+      this.props.onAddPlace(
+        this.state.controls.placeName.value,
+        this.state.controls.location.value
+      );
     };
 
     render() {
@@ -73,7 +92,7 @@ class SharePlaceScreen extends Component {
                 <HeadingText>Share a Place with us!</HeadingText>
               </MainText>
               <PickImage />
-              <PickLocation />
+              <PickLocation onLocationPick={this.locationPickedHandler}/>
               <PlaceInput
                 placeData={this.state.controls.placeName}
                 onChangeText={this.placeNameChangedHandler}
@@ -81,7 +100,10 @@ class SharePlaceScreen extends Component {
             <Button
               title="Share the Place!"
               onPress={this.placeAddedHandler}
-              disabled={!this.state.controls.placeName.valid}
+              disabled={
+                !this.state.controls.placeName.valid ||
+                !this.state.controls.location.valid
+              }
             />
             </View>
           </ScrollView>
