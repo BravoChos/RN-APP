e8bde1f66c98ad59ad9b9e90d8567937abc653e6
Song Cho <deeplearner507@gmail.com>
2019-05-27 03:36:45 +0900

    firebase config done

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,40 +1,48 @@
 import {ADD_PLACE, DELETE_PLACE } from './actionTypes';
 import axios from 'axios';
 
-export const addPlace = (placeName, location,image ) => async dispatch => {
-    const placeData = {
-        name: placeName,
-        location: location
-    };
-    console.log(image.content)
-    const res = await axios.post("https://my-project-1558248234390.firebaseio.com//places.json", placeData)
-    console.log("hello",res);
-    dispatch ({ type: ADD_PLACE, payload: res.data});
-
-};
-
-// export const addPlace = (placeName, location,image ) => {
+// export const addPlace = (placeName, location,image ) => async dispatch => {
 //     const placeData = {
 //         name: placeName,
 //         location: location
 //     };
-//     fetch("https://awesome-places-1511248766522.firebaseio.com/places.json", {
-//         method: "POST",
-//         body: JSON.stringify(placeData)
-//     })
-//     .catch(err => console.log(err))
-//     .then(res => res.json())
-//     .then(parsedRes => {
-//         console.log(parsedRes);
-//     });
+//     console.log(image.content)
+//     const res = await axios.post("https://my-project-1558248234390.firebaseio.com//places.json", placeData)
+//     console.log("hello",res);
+//     dispatch ({ type: ADD_PLACE, payload: res.data});
+
 // };
-    // return {
-    //     type: ADD_PLACE,
-    //     placeName: placeName,
-    //     location: location,
-    //     image: image
-    // };
-// };
+
+export const addPlace = (placeName, location, image) => {
+    return dispatch => {
+        fetch("https://us-central1-my-project-1558248234390.cloudfunctions.net/storeImage", {
+            method: "POST",
+            body: JSON.stringify({
+                image: image.base64
+            })
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes)
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            console.log(placeData)
+            return fetch("https://my-project-1558248234390.firebaseio.com//places.json", {
+                method: "POST",
+                body: JSON.stringify(placeData)
+            })
+        })  
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+        });
+    };
+};
 
 export const deletePlace = (key) => {
     return {
