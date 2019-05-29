91f42572a6a4e7a6131124e58a0b21dca8b77ee6
Song Cho <deeplearner507@gmail.com>
2019-05-30 00:04:00 +0900

    adding user login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,41 +1,44 @@
 import { TRY_AUTH } from './actionTypes';
-import {authUrl} from '../../../keys';
+import {apiKey} from '../../../keys';
 import { uiStartLoading, uiStopLoading } from "./index";
 import startMainTabs from "../../screens/MainTabs/startMainTabs";
 
-export const tryAuth = (authData) => {
+export const tryAuth = (authData, authMode) => {
     return dispatch => {
-        dispatch(authSignup(authData));
-    };
-};
-
-export const authSignup = (authData) => {
-    return dispatch => {
-        dispatch(uiStartLoading());
-        fetch(authUrl, {
+      dispatch(uiStartLoading());
+      let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + apiKey;
+      if (authMode === "signup") {
+          url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + apiKey
+      }
+      fetch(
+          url,
+          {
             method: "POST",
             body: JSON.stringify({
-                email: authData.email,
-                password: authData.password,
-                returnSecureToken: true
+              email: authData.email,
+              password: authData.password,
+              returnSecureToken: true
             }),
             headers: {
-                "Content-Type": "application/json"
+              "Content-Type": "application/json"
             }
-        })
-        .catch(err => {
+          }
+        )
+          .catch(err => {
             console.log(err);
             alert("Authentication failed, please try again!");
             dispatch(uiStopLoading());
-        })
-        .then(res => res.json())
-        .then(parsedRes => {
+          })
+          .then(res => res.json())
+          .then(parsedRes => {
             dispatch(uiStopLoading());
             if (parsedRes.error) {
-                alert("Authentication failed, please try again!");
+              alert("Authentication failed, please try again!");
             } else {
-                startMainTabs();
+              startMainTabs();
             }
-        });
+          });
     };
 };
+
+
